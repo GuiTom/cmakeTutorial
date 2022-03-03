afa544cdf5ff7487d5640748e1b50957e64eec3b
chenchao2 <chen.chao2@olaola.chat>
2022-03-03 11:12:21 +0800

    加入demo6

--- Demo3/build/CMakeFiles/Demo.dir/DependInfo.cmake
+++ Demo3/build/CMakeFiles/Demo.dir/DependInfo.cmake
@@ -13,6 +13,7 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/chenchao/Desktop/learnCmake/Demo3/build/math/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
---
+++ Demo5/build/math/CMakeFiles/MathFunctions.dir/MathFunctions.cc.o.d
@@ -0,0 +1,2 @@
+math/CMakeFiles/MathFunctions.dir/MathFunctions.cc.o: \
+  /Users/chenchao/Desktop/learnCmake/Demo5/math/MathFunctions.cc
